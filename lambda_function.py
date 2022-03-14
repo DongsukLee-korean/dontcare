72a7e4d7beb3021e52b8d9380592d5c5a58c0810
DongsukLee-korean <101540670+DongsukLee-korean@users.noreply.github.com>
2022-03-14 13:35:47 +0900

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import imutils
+import argparse
+import matplotlib
+matplotlib.use('Agg')
+
+def lambda_handler(event,context):
+  print('OpenCV 버전 = ', cv2.__version__)
+  print('numpy 버전 = ', np.__version__)
+  print('imutils 버전 = ', imutils.__version__)
+  print('argparse 버전 = ', argparse.__version__)
+  print('matplotlib 버전 = ', matplotlib.__version__)
